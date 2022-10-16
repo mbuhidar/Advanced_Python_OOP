afc18ebcc57187b02d8aee520bd9281180b7b6ad
Michael Buhidar <mbuhidar@gmail.com>
2022-10-16 11:07:59 -0400

    Finish App4 - Kivy Photo Sharer

--- App4_Kivy_Photo_Sharer/filesharer.py
+++ App4_Kivy_Photo_Sharer/filesharer.py
@@ -1,9 +1,13 @@
-# from filestack import Client
+from filestack import Client
+
+# You will need to create a config.py file in same directory with contents:
+# filestack_api_key = "YOUR API KEY HERE"
+import config
 
 
 class FileSharer:
 
-    def __init__(self, filepath, api_key="INSERT YOUR API KEY HERE"):
+    def __init__(self, filepath, api_key=config.filestack_api_key):
         self.filepath = filepath
         self.api_key = api_key
 
