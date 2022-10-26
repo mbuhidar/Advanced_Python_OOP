aaca4942f9db1fa8770a0042d0c75fada8c0fe68
Michael Buhidar <mbuhidar@gmail.com>
2022-10-26 06:41:01 -0400

    Finish calorie and temperature classes.

--- App6_Calories_App/calorie.py
+++ App6_Calories_App/calorie.py
@@ -1,3 +1,6 @@
+from temperature import Temperature
+
+
 class Calorie:
     """Represent number of calories calculated with the
     BMR = 10*weight + 6.25*height - 5*age + 5 - 10*temperature
@@ -10,4 +13,11 @@ class Calorie:
         self.temperature = temperature
 
     def calculate(self):
-        pass
+        return 10 * self.weight + 6.26 * self.height + \
+                    5 * self.age - 10 * self.temperature
+
+
+if __name__ == '__main__':
+    temperature = Temperature(country='italy', city='rome').get()
+    calorie = Calorie(weight=70, height=175, age=32, temperature=temperature)
+    print(calorie.calculate())
